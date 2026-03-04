4c1aa8b4cbaad07881e7eea0788a8b8f9b51492f
shad910 <shadshs91@gmail.com>
2026-03-04 19:09:37 +0600

    initial setup for mr2-ticket-system

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+
+  return (
+    <div className='flex flex-col'>
+      <header></header>
+      <main className='flex-1 my-10'></main>
+      <footer></footer>
+    </div>
+  )
+}
+
+export default App
