b57cf90a6f0fcc2e50770974fa54e9eef714e983
shad910 <shadshs91@gmail.com>
2026-03-05 13:23:05 +0600

    tickets.json data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Navbar from './shared/Navbar';
-import Footer from './shared/Footer';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
 
